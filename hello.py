2fec6f1d4f91d8b3735ffa4a5a6e1d037213bcc4
wangxiaoyangwz <1161652587@qq.com>
2017-05-14 15:34:51 +0800

    render_template

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
  # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask,render_template
 from flask_script import Manager#启动项设置，命令行解释器
 
 app = Flask(__name__)#初始化
@@ -7,12 +7,12 @@ manager = Manager(app)
 
 @app.route('/')
 def index():#视图函数-->生成响应
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')#将模板集成到程序
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html',name=name)#参数传到模版中
 
 
 if __name__ == '__main__':#启动服务器
