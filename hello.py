31e77998bcf532c173786e334616e853cfbc8477
wangxiaoyangwz <1161652587@qq.com>
2017-05-14 16:54:27 +0800

    Moment datetime.utcnow()/base->moment.include_moment/index->moment().format() fromNow(refresh=True)

--- hello.py
+++ hello.py
@@ -2,11 +2,14 @@
 from flask import Flask,render_template
 from flask_script import Manager#启动项设置，命令行解释器
 from flask_bootstrap import Bootstrap#bootstrap是客户端框架，提供用户界面组件，创建网页
+from flask_moment import Moment
+from datetime import datetime
 
 
 app = Flask(__name__)#初始化
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment=Moment(app)
 
 
 @app.errorhandler(404)#定义错误页面
@@ -21,7 +24,7 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():#视图函数-->生成响应
-    return render_template('index.html')#将模板集成到程序
+    return render_template('index.html'current_time=datetime.utcnow())#将模板集成到程序
 
 
 @app.route('/user/<name>')
