28b0c543d19bdfd3d871412131d185de8536fa93
wangxiaoyangwz <1161652587@qq.com>
2017-05-14 15:27:22 +0800

    Manager

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
  # -*- coding: utf-8 -*-
 from flask import Flask
-app = Flask(__name__)#初始化
+from flask_script import Manager#启动项设置，命令行解释器
 
+app = Flask(__name__)#初始化
+manager = Manager(app)
 
 @app.route('/')
 def index():#视图函数-->生成响应
@@ -14,4 +16,4 @@ def user(name):
 
 
 if __name__ == '__main__':#启动服务器
-    app.run(debug=True)
+    manater.run()
