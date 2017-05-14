db7eb13775f7e57df27b8cc223f23f96902f2604
wangxiaoyangwz <1161652587@qq.com>
2017-05-14 16:59:16 +0800

    bug

--- hello.py
+++ hello.py
@@ -12,15 +12,6 @@ bootstrap = Bootstrap(app)
 moment=Moment(app)
 
 
-@app.errorhandler(404)#定义错误页面
-def page_not_found(e):
-    return render_template('404.html'), 404
-    
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
 
 @app.route('/')
 def index():#视图函数-->生成响应
@@ -29,7 +20,16 @@ def index():#视图函数-->生成响应
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name=name)#参数传到模版中
+    return render_template('user.html',name=name)#参数传到模版中@app.errorhandler(404)#定义错误页面
+
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+    
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':#启动服务器
