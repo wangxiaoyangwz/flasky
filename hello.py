1b847df1d128c7f39d69860b0119b2e217e45ea1
wangxiaoyangwz <1161652587@qq.com>
2017-05-14 15:23:04 +0800

    user()

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
+ # -*- coding: utf-8 -*-
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__)#初始化
 
 
 @app.route('/')
-def index():
+def index():#视图函数-->生成响应
     return '<h1>Hello World!</h1>'
 
 
-if __name__ == '__main__':
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+if __name__ == '__main__':#启动服务器
     app.run(debug=True)
