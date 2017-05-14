5ec869592fbcd99d66bb9a72d5cbc6e303053704
wangxiaoyangwz <1161652587@qq.com>
2017-05-14 18:48:11 +0800

    SECRET_KEY/class NameForm/form.name.data/index.html wtf.quick_form

--- hello.py
+++ hello.py
@@ -1,22 +1,34 @@
+# -*- coding: utf-8 -*-
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
-from datetime import datetime
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'#设置密匙，app.config字典存储
+
 manager=Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+class NameForm(FlaskForm): #表单类
+    name = StringField('What is your name?', validators=[Required()])#属性有文本字段和submit按钮
+                                                                    #字段对象附属一个验证函数——>输入合法？
+                                                                      #validators是一个验证函数组成的列表
+    submit = SubmitField('Submit')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html',name=name)
+
+@app.route('/', methods=['GET', 'POST'])#methods视图函数注册为GET,POST请求的处理函数
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():#检测函数
+        name = form.name.data#name存放由data获取的输入名
+        form.name.data = ''#清空表单字段
+    return render_template('index.html', form=form, name=name)#表单实例，将其通过参数form传入模板，将data获得的输入名字传到模版中
 
 
 @app.errorhandler(500)
