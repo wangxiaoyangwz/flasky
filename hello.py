a4835b7cf82125717509dd086a2c9c20d3ac44f4
wangxiaoyangwz <1161652587@qq.com>
2017-05-14 16:18:22 +0800

    hell0->bootstrap/user.html->navbar

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
  # -*- coding: utf-8 -*-
 from flask import Flask,render_template
 from flask_script import Manager#启动项设置，命令行解释器
+from flask_bootstrap import Bootstrap#bootstrap是客户端框架，提供用户界面组件，创建网页
+
 
 app = Flask(__name__)#初始化
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():#视图函数-->生成响应
@@ -16,4 +19,4 @@ def user(name):
 
 
 if __name__ == '__main__':#启动服务器
-    manater.run()
+    manager.run()
