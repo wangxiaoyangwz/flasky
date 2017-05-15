33b2dc3a210a40bb56ceac619c92bd60ce85f624
wangxiaoyangwz <1161652587@qq.com>
2017-05-15 20:37:07 +0800

    git add .

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 import os
-from flask import Flask, render_template, session, redirect, url_for,flash
-from flask_script import Manager,Shell
+from flask import Flask, render_template, session, redirect, url_for
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -12,28 +11,27 @@ from flask_migrate import Migrate, MigrateCommand
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'#设置密匙，app.config字典存储
+app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')#程序使用的数据库的URL保存在配置对象SQLALCHEMY_DATABASE_URI键
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #请求结束时自动提交数据库的变动
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-db = SQLAlchemy(app)#SQLAlchemy类的实例，表示程序使用的数据库，获得flask-sqlalchemy所有功能
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+
 
 class Role(db.Model):
-    __tablename__ = 'roles'#数据库中使用的表名
-    id = db.Column(db.Integer, primary_key=True)#db.Column（1.类型类似于int，float，2.属性配置选型-主键，重复，等）
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')#user属性，'User'->Role与那个模型关联，返回与Role相关联的User对象的列表 (对象.属性.过滤器（）)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def __repr__(self):#
+    def __repr__(self):
         return '<Role %r>' % self.name
 
 
@@ -46,45 +44,44 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-class NameForm(FlaskForm): #表单类
-    name = StringField('What is your name?', validators=[Required()])#属性有文本字段和submit按钮
-                                                                    #字段对象附属一个验证函数——>输入合法？
-                                                                      #validators是一个验证函数组成的列表
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
 
-def make_shell_context():   #不懂！为shell命令添加一个上下文
-    return dict(app=app,db=db,User=User,Role=Role) #右边app是指hello.py中的实例对象，左边app是自己命名，以后可以在命令行中输入，就调用了app实例
-    
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-#make_context参数是规定要传入的上下文环境
+manager.add_command('db', MigrateCommand)
 
-
-@app.route('/', methods=['GET', 'POST'])#methods视图函数注册为GET,POST请求的处理函数
-def index():
-    form = NameForm()
-    if form.validate_on_submit():       #检测函数
-        user = User.query.filter_by(username=form.name.data).first()#在数据库中加载用户名为form.name.data的用户
-        if user is None:#若数据库中不存在用户名为form.name.data的用户
-            user = User(username=form.name.data)#那便创建是改名字的用户
-            db.session.add(user)#将该用户对象写入会话中
-            session['known'] = False
-        else:
-            session['known'] = True#若已经存在，根据该值得不同可显示不同的欢迎消息
-        session['name'] = form.name.data #然后在将新输入的名字存储在用户会话中
-        return redirect(url_for('index')) #刷新后，重定向URL发出GET请求，只显示网页，重新打POST请求，刷新会再次提交表单
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))#表单实例，将其通过参数form传入模板，将data获得的输入名字传到模版中  #session.get()获取存储在用户会话名字
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'),500
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
 
 
-if __name__=='__main__':
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
+
+
+if __name__ == '__main__':
     manager.run()
-
