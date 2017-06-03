0823ad551233f94bf8ce97871f4f46d96595a698
unknown <Administrator@PC-20170531MINH.(none)>
2017-06-03 23:07:17 +0800

    profile_articles

--- app/main/views.py
+++ app/main/views.py
@@ -27,6 +27,7 @@ def user(username):
 	user=User.query.filter_by(username=username).first()
 	if user is None:
 		abort(404)
+	posts=user.posts.order_by(Post.timestamp.desc()).all()#从数据库中查询，并按照时间排序
 	return render_template('user.html',user=user)
 
 
